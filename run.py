089cd23605597fad6db931162b0818f694181118
WH0ISWHO <jayden0kim@gmail.com>
2025-02-04 00:17:58 -0800

    Fixed run.py

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 from app import create_app, db
-from app.days import days
+# from app.days import days
 from app.models import Day
 from app.utils import insert_data, delete_data_by_day
 
 app = create_app()
 
-with app.app_context():
-    print("Creating tables...")
-    db.create_all()  # create a table of db
-    print("Tables created successfully!")
+# with app.app_context():
+#     print("Creating tables...")
+#     db.create_all()  # create a table of db
+#     print("Tables created successfully!")
 
-    new_data = len(days)
-    existing_data = Day.query.count()
+#     new_data = len(days)
+#     existing_data = Day.query.count()
 
-    if existing_data != new_data:  # execute only if Day's table is empty
-        print("Inserting data...")
-        insert_data(days)
-        print("Setup complete!")
-    else:
-        print("Data already exists. Skipping insertion.")
+#     if existing_data != new_data:  # execute only if Day's table is empty
+#         print("Inserting data...")
+#         insert_data(days)
+#         print("Setup complete!")
+#     else:
+#         print("Data already exists. Skipping insertion.")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3333)
