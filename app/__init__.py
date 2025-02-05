6cb690ee729a9caad76a238549d1ed4ad52f3654
WH0ISWHO <jayden0kim@gmail.com>
2025-02-05 14:49:50 -0800

    Deploy test

--- app/__init__.py
+++ app/__init__.py
@@ -8,23 +8,17 @@ import os
 db = SQLAlchemy()
 migrate = Migrate()
 
+from dotenv import load_dotenv
 def create_app():
-    from dotenv import load_dotenv
     load_dotenv()
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI') + os.path.join(app.instance_path, 'vocabulary.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
-
     db.init_app(app)
     migrate.init_app(app, db)
-
-    db_path = os.path.join("instance", "vocabulary.db")
-    if not os.path.exists(db_path):
-        with open(db_path, "w") as f:
-            pass
-        db.create_all()
 
     from .views import views_bp
     # prefixing within the address bar/url info to show a certain string. Leave blank if you dont want.
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from app import create_app, db
 # from app.days import days
 from app.models import Day
 from app.utils import insert_data, delete_data_by_day
+from flask_migrate import upgrade
 
 app = create_app()
 
@@ -20,17 +21,22 @@ app = create_app()
 #     else:
 #         print("Data already exists. Skipping insertion.")
 
-
-import os
-db_path = os.path.join("instance", "vocabulary.db")
-
-if not os.path.exists(db_path):
-    with open(db_path, "w") as f:
-        pass
-
-    # DB
-    with app.app_context():
-        db.create_all()
-
 if __name__ == '__main__':
+    # with app.app_context():
+
+    #     print("Running migrations...")
+    #     upgrade()
+    #     print("Migrations complete!")
+
+    #     new_data = len(days)
+    #     existing_data = Day.query.count()
+
+    #     # execute only if Day's table is empty
+    #     if existing_data != new_data:
+    #         print("Inserting data...")
+    #         insert_data(days)
+    #         print("Setup complete!")
+    #     else:
+    #         print("Data already exists. Skipping insertion.")
+
     app.run(debug=True, port=3333)
